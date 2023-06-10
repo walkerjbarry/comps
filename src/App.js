9f4dd22981124c021f80b0d56359a1606452c8ad
bw_esc13 <barry.walker@esc13.txed.net>
2023-06-10 12:34:42 -0500

    added Contextdirector, as well as Navigation.js, began wiring up the navivgation.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,9 @@
 
-import { useState } from 'react';
-import Dropdown from './components/Dropdown';
-
 
 function App() {
-    const [selection, setSelection] = useState(null);
+   
 
-    const handleSelect = (option) => {
-        setSelection(option);
-    };
-
-    const options = [
-        { label: 'Red', value: 'red' },
-        { label: 'Green', value: 'green' },
-        { label: 'Blue', value: 'blue' },
-    ];
-
-    return (
-        <Dropdown
-            options={options}
-            value={selection}
-            onChange={handleSelect} />
-    )
-};
+    return <div></div>;
+}
 
 export default App;
